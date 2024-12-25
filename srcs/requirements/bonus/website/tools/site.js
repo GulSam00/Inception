288a7cd12041857cf02295d6541de4fb76bc0178
gulsam00 <gulsamcono@gmail.com>
2024-12-25 13:44:20 +0900

    feat : 디자인 추가. css 조정

--- srcs/requirements/bonus/website/tools/site.js
+++ srcs/requirements/bonus/website/tools/site.js
@@ -1,9 +1,17 @@
 const http = require('http')
 const fs = require('fs')
+const path = require('path')
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'content-type': 'text/html' })
-  fs.createReadStream('index.html').pipe(res)
+  if (req.url === '/style.css') {
+    // CSS 파일 요청 처리
+    res.writeHead(200, { 'Content-Type': 'text/css' })
+    fs.createReadStream('style.css').pipe(res)
+  } else {
+    // 기본적으로 index.html 반환
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    fs.createReadStream('index.html').pipe(res)
+  }
 })
 
 server.listen(process.env.PORT || 7777)
