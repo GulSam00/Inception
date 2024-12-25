2bb88cf02bb394adfcf0252187433cc0a3edda3f
gulsam00 <gulsamcono@gmail.com>
2024-12-25 15:29:56 +0900

    feat : nginx 라우팅 구현

--- srcs/requirements/bonus/website/tools/site.js
+++ srcs/requirements/bonus/website/tools/site.js
@@ -3,15 +3,8 @@ const fs = require('fs')
 const path = require('path')
 
 const server = http.createServer((req, res) => {
-  if (req.url === '/style.css') {
-    // CSS 파일 요청 처리
-    res.writeHead(200, { 'Content-Type': 'text/css' })
-    fs.createReadStream('style.css').pipe(res)
-  } else {
-    // 기본적으로 index.html 반환
-    res.writeHead(200, { 'Content-Type': 'text/html' })
-    fs.createReadStream('index.html').pipe(res)
-  }
+  res.writeHead(200, { 'Content-Type': 'text/html' })
+  fs.createReadStream('index.html').pipe(res)
 })
 
 server.listen(process.env.PORT || 7777)
